39a01159714079d61b3e28321cb421aaa60d506f
Jacob <Jacob@192.168.2.6>
2018-04-04 02:41:08 -0500

    Cleaned up formatting and added comments to Capstone.java and LoginScreen.java.

--- src/gui/LoginScreen.java
+++ src/gui/LoginScreen.java
@@ -1,74 +1,88 @@
-package gui;
 
+//Imports
+package gui;
 import java.util.Scanner;
 import java.io.*;
 import javax.swing.*;
-
 import controllers.DefaultController;
 import db.AssignmentQueries;
 import db.ConfirmCredentials;
 import user.Teacher;
-
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+//________________________________________________________LOGINSCREEN________________________________________________________
 public class LoginScreen
 {
-	private static JTextField usernameField;
-	private static JTextField passwordField;
+	//Variables
+	private static JTextField usernameField;	//This variable retrieves the value from our username field entry.
+	private static JTextField passwordField;	//This variable retrieves the value from our password field entry.
 
+	//---------------------------------------------------------DISPLAY---------------------------------------------------------
+	//This is the display method that is called from other methods to open the login screen.
 	/**
 	 * @wbp.parser.entryPoint
 	 */
 	public static void display()
 	{
+		//Jframe
 		JFrame frmLogin = new JFrame("Login");
 
 	    frmLogin.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	    frmLogin.setSize(811,627);  
 	    frmLogin.getContentPane().setLayout(null);
 	    
+	    //Username Label
 	    JLabel lblUsername = new JLabel("Username:");
 	    lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 20));
 	    lblUsername.setBounds(322, 366, 119, 28);
 	    frmLogin.getContentPane().add(lblUsername);
 	    
+	    //Username Textfield
 	    usernameField = new JTextField();
 	    usernameField.setFont(new Font("Tahoma", Font.PLAIN, 20));
 	    usernameField.setBounds(452, 366, 196, 28);
 	    frmLogin.getContentPane().add(usernameField);
 	    usernameField.setColumns(10);
 	    
+	    //Password Label
 	    JLabel lblPassword = new JLabel("Password:");
 	    lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 20));
 	    lblPassword.setBounds(322, 405, 119, 28);
 	    frmLogin.getContentPane().add(lblPassword);
 	    
+	    //Password Textfield
 	    passwordField = new JTextField();
 	    passwordField.setFont(new Font("Tahoma", Font.PLAIN, 20));
 	    passwordField.setColumns(10);
 	    passwordField.setBounds(452, 405, 196, 28);
 	    frmLogin.getContentPane().add(passwordField);
 	    
+	    //Submit Button
 	    JButton btnSubmit = new JButton("Submit");
-	    btnSubmit.addActionListener(new ActionListener() {
-	    	public void actionPerformed(ActionEvent e) {
+	    btnSubmit.addActionListener(new ActionListener()
+	    {
+	    	//Button has been clicked.
+	    	public void actionPerformed(ActionEvent e)
+	    	{
+	    		//Get text from username and password fields.
 	    		String username = usernameField.getText();
 				String userPassword = passwordField.getText();
 				
-				//User has submitted their userID and password.
+				//Send the username and password to the credentials method of the ConfirmCredentials class.
 				ConfirmCredentials.credentials(username, userPassword);
+	    	    frmLogin.setVisible(false);
 	    	}
-	    	
 	    });
 	    btnSubmit.setBounds(515, 454, 89, 23);
 	    frmLogin.getContentPane().add(btnSubmit);
 	    
-	    JLabel label = new JLabel("");
-	    label.setIcon(new ImageIcon(LoginScreen.class.getResource("/images/Login.png")));
-	    label.setBounds(0, 0, 803, 588);
-	    frmLogin.getContentPane().add(label);
+	    //Background Image
+	    JLabel backgroundImage = new JLabel("");
+	    backgroundImage.setIcon(new ImageIcon(LoginScreen.class.getResource("/images/Login.png")));
+	    backgroundImage.setBounds(0, 0, 803, 588);
+	    frmLogin.getContentPane().add(backgroundImage);
 	    frmLogin.setVisible(true);
 	}
 }
--- src/main/Capstone.java
+++ src/main/Capstone.java
@@ -1,43 +1,32 @@
 package main;
 
-//_________________________MAIN PROGRAM_____________________________
+//Imports
 import gui.*;
 import user.*;
-
 import java.util.Scanner;
 import java.io.*;
 import javax.swing.*;
 import java.awt.*;
-
-//This utility allows the program to connect to the internet.
-//This is used for displaying the welcome-screen image.
 import java.net.*;
 
-//PROGRAM
+//________________________________________________________CAPSTONE (MAIN PROGRAM)________________________________________________________
 public class Capstone
 {
-	//---------------------------------MAIN----------------------------------------
+	//---------------------------------------------------------MAIN---------------------------------------------------------
 	public static void main(String[] args) throws IOException
 	{
-		/*
-		 * uncomment 'welcomescreen();' to run program normally.
-		 * For development and testing purposes, I'm running another method
-		 */
-		
+		//DEVELOPMENT AND TESTING PURPOSES:
+		//(((Uncomment below to launch.)))
+		//--------------------------------------------------------
 		//TestFunctionsWithoutGUI program = new TestFunctionsWithoutGUI();
 		//program.test();
 		
-		
-		//Allowing keyboard input.
-		//Scanner keyboard = new Scanner(System.in);
-		
-		//Welcome Screen
+		//Display Login Screen
 		LoginScreen.display();
 		
-		/*
-		 * to launch an action center directly, comment out welcomeScreen() above,
-		 * and uncomment the action center you want the program to launch
-		 */
+		//ACTION CENTER TESTING:
+		//(((Uncomment action center below to launch it directly.)))
+		//--------------------------------------------------------
 		//AdminActionCenter.display(new Administrator(1));
 		//StudentActionCenter.display(new Student(1));
 		//TeacherActionCenter.display(new Teacher(1));
